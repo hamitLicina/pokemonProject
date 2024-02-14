d04e354090dcc645d3acf2cc0cf23596f82c4f5d
Hamit LİÇİNA <126760278+hamitLicina@users.noreply.github.com>
2024-02-14 08:44:21 +0300

    After bootcamp main changes added and unnecessary parts deleted

--- script.js
+++ script.js
@@ -6,25 +6,25 @@ const pokeHeight = document.getElementById("height");
 const abilitiesList = document.getElementById("abilities-list");
 const getPokeBtn = document.getElementById("get-poke-button");
 
-getPokeBtn.addEventListener("click", function() {
+getPokeBtn.addEventListener("click", function () {
 
-const randomPokeId = Math.floor(Math.random() * 1000 + 1);
+  const randomPokeId = Math.floor(Math.random() * 1000 + 1);
 
-fetch(`https://pokeapi.co/api/v2/pokemon/${randomPokeId}`)
-.then((res) => res.json())
-.then((json) => {
-  console.log(json);
-  const pokemon = json;
-  const pokeImgSrc = pokemon.sprites.other["official-artwork"].front_default;
-  console.log(pokeImgSrc);
-  mainImg.src = pokeImgSrc;
-  pokeName.innerText = `Name: ${pokemon.name}`;
-  pokeSpecies.innerText = `Species: ${pokemon.species.name}`;
-  pokeWeight.innerText = `Weight: ${pokemon.weight}`;
-  pokeHeight.innerText = `Height: ${pokemon.height}`;
-  let htmlString = "";
-  pokemon.abilities.forEach((element) => (htmlString += `<li>${element.ability.name}</li>`));
-  abilitiesList.innerHTML = htmlString;
-})
-.catch((err) => console.log(err));
+  fetch(`https://pokeapi.co/api/v2/pokemon/${randomPokeId}`)
+    .then((res) => res.json())
+    .then((json) => {
+      console.log(json);
+      const pokemon = json;
+      const pokeImgSrc = pokemon.sprites.other["official-artwork"].front_default;
+      console.log(pokeImgSrc);
+      mainImg.src = pokeImgSrc;
+      pokeName.innerText = `Name: ${pokemon.name}`;
+      pokeSpecies.innerText = `Species: ${pokemon.species.name}`;
+      pokeWeight.innerText = `Weight: ${pokemon.weight}`;
+      pokeHeight.innerText = `Height: ${pokemon.height}`;
+      let htmlString = "";
+      pokemon.abilities.forEach((element) => (htmlString += `<li>${element.ability.name}</li>`));
+      abilitiesList.innerHTML = htmlString;
+    })
+    .catch((err) => console.log(err));
 })
